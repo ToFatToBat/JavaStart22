07a43bcdeb6f5cb9038412bf4b6be9596fefa845
ToFatToBat <kpapiernia@wp.pl>
2020-08-19 21:47:56 +0200

    Budowa logiki - 2

--- src/main/java/com/tofattobat/firstspring/repository/ProductRepository.java
+++ src/main/java/com/tofattobat/firstspring/repository/ProductRepository.java
@@ -1,4 +1,4 @@
-package com.tofattobat.firstspring.Repository;
+package com.tofattobat.firstspring.repository;
 
 import com.tofattobat.firstspring.model.Product;
 
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class ProductRepository {
 
-   private List<Product> products;
+    private List<Product> products;
 
     public ProductRepository(List<Product> products) {
         products = new ArrayList<>();
@@ -18,10 +18,11 @@ public class ProductRepository {
         products.add(new Product("Patelnia - Tefal", 43.99, "Art. gosp. domowego"));
         products.add(new Product("Philips - Blender", 150, "Art. gosp. domowego"));
         products.add(new Product("Noze - Fiskars", 150, "Art. gosp. domowego"));
-        products.add(new Product("Bluza - FOX", 150, "Inne"));
+        products.add(new Product("Bluza - FOX", 180, "Inne"));
         products.add(new Product("Okulary", 150, "Inne"));
-        products.add(new Product("Torebka", 150, "Inne"));
-        products.add(new Product("Szpilki", 150, "Inne"));
-
+        products.add(new Product("Torebka", 450, "Inne"));
+        products.add(new Product("Szpilki", 649.99, "Inne"));
     }
+
+    public List<Product> listAll() {return new ArrayList<>(products);}
 }
