ad383d665b06b9133ec8b7c12846a12444eb76a2
ToFatToBat <kpapiernia@wp.pl>
2020-08-22 19:58:39 +0200

    /repository - poprawa przypisania do listy

--- src/main/java/com/tofattobat/firstspring/repository/ProductRepository.java
+++ src/main/java/com/tofattobat/firstspring/repository/ProductRepository.java
@@ -11,19 +11,19 @@ public class ProductRepository {
 
     private List<Product> products;
 
-    public ProductRepository(List<Product> product) {
-        product = new ArrayList<>();
-        product.add(new Product("Pudliszki", 5.55, "Art. spozywcze"));
-        product.add(new Product("Heinz", 6.20, "Art. spozywcze"));
-        product.add(new Product("Dawtona", 4.99, "Art. spozywcze"));
-        product.add(new Product("Pepsi", 5.99, "Art. spozywcze"));
-        product.add(new Product("Patelnia - Tefal", 43.99, "Art. gosp. domowego"));
-        product.add(new Product("Philips - Blender", 150, "Art. gosp. domowego"));
-        product.add(new Product("Noze - Fiskars", 150, "Art. gosp. domowego"));
-        product.add(new Product("Bluza - FOX", 180, "Inne"));
-        product.add(new Product("Okulary", 150, "Inne"));
-        product.add(new Product("Torebka", 450, "Inne"));
-        product.add(new Product("Szpilki", 649.99, "Inne"));
+    public ProductRepository() {
+
+        products.add (new Product("Pudliszki", 5.55, "Art. spozywcze"));
+        products.add (new Product("Heinz", 6.20, "Art. spozywcze"));
+        products.add (new Product("Dawtona", 4.99, "Art. spozywcze"));
+        products.add (new Product("Pepsi", 5.99, "Art. spozywcze"));
+        products.add (new Product("Patelnia - Tefal", 43.99, "Art. gosp. domowego"));
+        products.add (new Product("Philips - Blender", 150, "Art. gosp. domowego"));
+        products.add (new Product("Noze - Fiskars", 150, "Art. gosp. domowego"));
+        products.add (new Product("Bluza - FOX", 180, "Inne"));
+        products.add (new Product("Okulary", 150, "Inne"));
+        products.add (new Product("Torebka", 450, "Inne"));
+        products.add (new Product("Szpilki", 649.99, "Inne"));
     }
 
     public List<Product> listAll() {return new ArrayList<>(products);}
